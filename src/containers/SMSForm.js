a9cb1781ce8aa043d8851bf2a117227b0fceba9d
TB831 <druu831@gmail.com>
2018-12-26 13:45:39 -0800

    SMSFrom return complete

--- src/containers/SMSForm.js
+++ src/containers/SMSForm.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import SMSForm from './SMSForm';
 
 class SMSForm extends Component {
     state = {}
     render() { 
-        return (  );
+        return (
+            <form>
+                <div>
+                <label htmlFor="to">To:</label>
+                <input
+                    type="tel"
+                    name="to"
+                    id="to"
+                />
+                </div>
+                <div>
+                <label htmlFor="body">Body:</label>
+                <textarea name="body" id="body"/>
+                </div>
+                <button type="submit">
+                Send message
+                </button>
+            </form>
+        );
     }
 }
  
