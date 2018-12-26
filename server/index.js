08211a27b2e5581a17ec9612f62010a679be54aa
TB831 <druu831@gmail.com>
2018-12-26 13:15:43 -0800

    api endpoint complete

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const pino = require('express-pino-logger')();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(pino);
 
 const port = process.env.PORT || 3001;
@@ -15,6 +16,23 @@ const client = require('twilio')(
 
 app.get('/', (req, res) => {
   res.send('Hello World')
+});
+
+app.post('/api/messages', (req, res) => { // Twilio api endpoint
+  res.header('Content-Type', 'application/json');
+  client.messages
+    .create({
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: req.body.to,
+      body: req.body.body
+    })
+    .then(() => {
+      res.send(JSON.stringify({ success: true }));
+    })
+    .catch(err => {
+      console.log(err);
+      res.send(JSON.stringify({ success: false }));
+    });
 });
 
 app.listen(port, () => {
